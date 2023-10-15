64f9c5122b0a199eeb9a7fb2e02e0f2ca65f3bf1
Michal Hradis <michal.hradis@gmail.com>
2023-10-15 16:53:51 +0200

    Frontend can record. Backend can not receive yet.

--- backend/semant_annotation/routes/__init__.py
+++ backend/semant_annotation/routes/__init__.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 user_route = APIRouter()
 news_route = APIRouter()
 task_route = APIRouter()
+audio_route = APIRouter()
 
 from .user_routes import user_route
 from .news_routes import news_route
 from .task_routes import task_route
+from .audio_routes import audio_route
