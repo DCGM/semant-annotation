598d337554c6050e5da1cb3d7757bf40a1f96524
xbalca11 <xbalca11@DESKTOP-Q43QPLT>
2023-11-16 19:24:08 +0100

    fixed time tracking, output is now csv, added all times

--- backend/scripts/stats.py
+++ backend/scripts/stats.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 import json
 import sys
+import csv
 from datetime import datetime, timedelta
 
 def parse_args():
@@ -12,6 +13,7 @@ def parse_args():
     parser.add_argument('-s', '--start-time', default='2000-01-01T00:00:00', help='Start time.')
     parser.add_argument('-e', '--end-time', default='3000-01-01T00:00:00', help='End time.')
     parser.add_argument('-k', '--keypress-time', default='120', help='Time between keypress in seconds')
+    parser.add_argument('-a', '--api-link', default='http://pchradis2.fit.vutbr.cz:8000/api/', help='API link')
     return parser.parse_args()
 
 def get_session(url, session, login, password):
@@ -46,6 +48,11 @@ def format_date(raw_date):
 def format_date_ms(raw_date):
     return datetime.strptime(raw_date, "%Y-%m-%dT%H:%M:%S.%fZ")
 
+def format_result_date(raw_date): 
+    if len(raw_date) == 19: # sometimes we get 2023-11-16T15:54:12
+        raw_date = raw_date + ".000000"
+    return datetime.strptime(raw_date, "%Y-%m-%dT%H:%M:%S.%f")
+
 def format_time_tracking_url(url, start_time, end_time, user_id):
     start_time = start_time.replace(':', '%3A')
     end_time = end_time.replace(':', '%3A')
@@ -76,33 +83,35 @@ def time_tracking_users(session, user, time_tracking_url, start_time, end_time):
 
     return user_record_to_time(user_record)
 
-def time_task(data, keypress_time):
+def time_task(data, start_time, end_time, keypress_time):
     time = float(0)
     all_timestamps = []
+
     for key, values in data.items():
         for entry in values:
             history = entry.get("history", [])
             timestamps = [record["timestamp"] for record in history]
             all_timestamps.extend(timestamps)
 
-    if all_timestamps:
-        previous_timestamp = format_date_ms((all_timestamps[0]))
+    previous_timestamp = format_result_date(start_time) # add start time
     
+    all_timestamps.sort() # sort times
+
     for timestamp in all_timestamps:
-        print(timestamp)
         time_difference = format_date_ms(timestamp) - previous_timestamp
         if time_difference.total_seconds() <= keypress_time:
-            if time_difference.total_seconds() >= 0:
-                time += time_difference.total_seconds() 
+            time += time_difference.total_seconds() 
         previous_timestamp = format_date_ms(timestamp)
     
-    print(time)
+    end_difference = format_result_date(end_time) - previous_timestamp
+    if end_difference.total_seconds() <= keypress_time:  # add end time
+        time += end_difference.total_seconds()
+
     return time
 
 def seconds_to_time(seconds):
-    hours, remainder = divmod(seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
+    hours = seconds / 3600
+    return "{:.2f}h".format(hours)
 
 def get_final_list(users, tasks, start_time, end_time, keypress_time, task_result_url, time_tracking_url, session):
     list = []
@@ -114,6 +123,7 @@ def get_final_list(users, tasks, start_time, end_time, keypress_time, task_resul
         timed_data = {"full_name" : user["full_name"],
                       "email"     : user["email"]
                       }
+        time_all_tasks = 0
         for task in tasks:
             data = {
                     "annotation_task_id": task["id"],
@@ -131,10 +141,12 @@ def get_final_list(users, tasks, start_time, end_time, keypress_time, task_resul
             time_total = 0
             
             for item in raw_tasks_result:
-                task_timed = time_task(json.loads(item["result"]), keypress_time)
+                task_timed = time_task(json.loads(item["result"]), item["start_time"], item["end_time"], keypress_time)
                 time_total += task_timed
             timed_data[task["name"]] = seconds_to_time(time_total)
+            time_all_tasks += time_total
         
+        timed_data["all_times"] = seconds_to_time(time_all_tasks)
         user_record = time_tracking_users(session, user, time_tracking_url, start_time, end_time)
         merged_dir = timed_data.copy()
         merged_dir.update(user_record)
@@ -144,19 +156,20 @@ def get_final_list(users, tasks, start_time, end_time, keypress_time, task_resul
         
 
 def main():
-    login_url = 'http://pchradis2.fit.vutbr.cz:8000/api/token'
-    user_url = 'http://pchradis2.fit.vutbr.cz:8000/api/user/'
-    task_url = 'http://pchradis2.fit.vutbr.cz:8000/api/task/task'
-    task_result_url = 'http://pchradis2.fit.vutbr.cz:8000/api/task/results'
-    time_tracking_url = 'http://pchradis2.fit.vutbr.cz:8000/api/time_tracking/time_tracking'
-    
     args = parse_args()
     file = args.file
     username = args.login
     password = args.password
     start_time = args.start_time
     end_time = args.end_time
+    link = args.api_link
     keypress_time = int(args.keypress_time)
+
+    login_url = link + 'token'
+    user_url = link + 'user/'
+    task_url = link + 'task/task'
+    task_result_url = link + 'task/results'
+    time_tracking_url = link + 'time_tracking/time_tracking'
 
     session = requests.Session()
 
@@ -166,7 +179,6 @@ def main():
         print("Couldn't estabilish connection")
         sys.exit(1) 
 
-
     users = get_result(user_url, session, None)
 
     raw_tasks = get_result(task_url, session, None)
@@ -174,10 +186,15 @@ def main():
     
     final_list = get_final_list(users, tasks, start_time, end_time, keypress_time, task_result_url, time_tracking_url, session)
 
-    with open(file, "w") as outfile:
-        for user in final_list:
-            json.dump(user, outfile)
-            print("",file=outfile)
+    field_names = []
+    field_names_dict = max(final_list,key=len)
+    for key in field_names_dict:
+        field_names.append(key)
+
+    with open(file, 'w') as csvfile: 
+        writer = csv.DictWriter(csvfile, fieldnames = field_names) 
+        writer.writeheader() 
+        writer.writerows(final_list) 
 
 if __name__ == '__main__':
     main()
