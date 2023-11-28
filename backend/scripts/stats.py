d1f215a857e647ca93bcd14cc985b4e9ec65427d
Michal Hradis <michal.hradis@gmail.com>
2023-11-28 13:23:19 +0100

    Updated stats.py - time tracking and timestamps from and to are consistent.

--- backend/scripts/stats.py
+++ backend/scripts/stats.py
@@ -1,79 +1,87 @@
+import logging
+
 import requests
 import argparse
 import json
 import sys
 import csv
 from datetime import datetime, timedelta
+import urllib.parse
+from urllib.parse import urljoin
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--login', default='xbalca11', help='Username.')
-    parser.add_argument('-f', '--file', help='Destination JSON file.', required=True)
+    parser.add_argument('-l', '--login', required=True, help='Username.')
+    parser.add_argument('-o', '--output', required=True, help='Destination JSON file.')
     parser.add_argument('-p', '--password', required=True, help='')
-    parser.add_argument('-s', '--start-time', default='2000-01-01T00:00:00', help='Start time.')
-    parser.add_argument('-e', '--end-time', default='3000-01-01T00:00:00', help='End time.')
-    parser.add_argument('-k', '--keypress-time', default='120', help='Time between keypress in seconds')
-    parser.add_argument('-a', '--api-link', default='http://pchradis2.fit.vutbr.cz:8000/api/', help='API link')
+    parser.add_argument('-s', '--start-date', default='2000-01-01', help='Results will include this date.')
+    parser.add_argument('-e', '--end-date', default='3000-01-0', help='Results will include this date.')
+    parser.add_argument('-k', '--keypress-time', default='180', help='Time between keypress in seconds')
+    parser.add_argument('-u', '--api-url', required=True, help='API URL.')
     return parser.parse_args()
 
+
 def get_session(url, session, login, password):
-    response = session.post(url, data={"username": login , "password": password})
+    response = session.post(url, data={"username": login, "password": password})
     
     if response.status_code != 200:
-        session = None
+        logging.error(f'Login failed.')
+        raise Exception('Login failed.')
 
     return session
 
-def get_result(url, session, data):
+
+def query_api(url, session, data):
     if data is None:
         response = session.get(url)
     else:
         response = session.post(url, json=data)
-    result = None
-    
-    if response.status_code == 200:
-        result = response.json()
-    return result
 
-# Filter active tasks with only id and name
-def filter_tasks(raw_tasks):
-    if raw_tasks is None:
-        return []
-    tasks = [{'id': item['id'], 'name': item['name']} for item in raw_tasks if item['active'] == True] 
-    return tasks
+    if response.status_code != 200:
+        logging.error(f'Error while getting result from {url}. {response.status_code} {response.reason} {response.text}')
+        raise Exception(f'Error while getting result from {url}. {response.status_code} {response.reason} {response.text}')
+
+    return response.json()
+
 
 def format_date(raw_date):
     return datetime.strptime(raw_date, "%Y-%m-%dT%H:%M:%S")
 
+
 def format_date_ms(raw_date):
     return datetime.strptime(raw_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+
 
 def format_result_date(raw_date): 
     if len(raw_date) == 19: # sometimes we get 2023-11-16T15:54:12
         raw_date = raw_date + ".000000"
     return datetime.strptime(raw_date, "%Y-%m-%dT%H:%M:%S.%f")
 
+
 def format_time_tracking_url(url, start_time, end_time, user_id):
-    start_time = start_time.replace(':', '%3A')
-    end_time = end_time.replace(':', '%3A')
+    start_time = urllib.parse.quote(start_time)
+    end_time = urllib.parse.quote(end_time)
     url = url + '?' + 'from_time=' + start_time + '&to_time=' + end_time + '&user_id=' + user_id
     return url
+
 
 def user_record_to_time(user_record):
     for key, value in user_record.items():
         if key != 'user_id':
-            user_record[key] = seconds_to_time(value)
+            user_record[key] = seconds_to_formatted_hours(value)
     return user_record
+
 
 def time_tracking_users(session, user, time_tracking_url, start_time, end_time):
     user_tracking_time_url = format_time_tracking_url(time_tracking_url, start_time, end_time, user['id'])
-    user_time_tracking = get_result(user_tracking_time_url, session, None)
+    user_time_tracking = query_api(user_tracking_time_url, session, None)
     
-    user_record = {'user_id' : user['id']}
+    user_record = {'user_id': user['id']}
 
     for record in user_time_tracking:
         time_spent = format_date(record['end_time']) - format_date(record['start_time'])
-        if time_spent.days < 0: # if negative just zero
+        if time_spent.days < 0:  # if negative just zero
             time_spent = timedelta()
         
         if record['task'] in user_record:
@@ -82,6 +90,7 @@ def time_tracking_users(session, user, time_tracking_url, start_time, end_time):
             user_record[record['task']] = time_spent.seconds
 
     return user_record_to_time(user_record)
+
 
 def time_task(data, start_time, end_time, keypress_time):
     time = float(0)
@@ -109,15 +118,13 @@ def time_task(data, start_time, end_time, keypress_time):
 
     return time
 
-def seconds_to_time(seconds):
-    hours = seconds / 3600
-    return "{:.2f}h".format(hours)
 
-def get_final_list(users, tasks, start_time, end_time, keypress_time, task_result_url, time_tracking_url, session):
+def seconds_to_formatted_hours(seconds):
+    return "{:.2f}".format(seconds / 3600)
+
+
+def get_final_list(users, tasks, start_date, end_date, keypress_time, task_result_url, time_tracking_url, session):
     list = []
-
-    if users is None:
-        return list
 
     for user in users:
         timed_data = {"full_name" : user["full_name"],
@@ -127,27 +134,22 @@ def get_final_list(users, tasks, start_time, end_time, keypress_time, task_resul
         for task in tasks:
             data = {
                     "annotation_task_id": task["id"],
-                    "from_date": start_time,
-                    "to_date": end_time,
+                    "from_date": start_date,
+                    "to_date": end_date,
                     "user_id": user["id"]
                     }
 
-            raw_tasks_result = get_result(task_result_url, session, data)
-
-            if raw_tasks_result is None:
-                timed_data[task["name"]] = seconds_to_time(0)
-                break
+            tasks_response = query_api(task_result_url, session, data)
 
             time_total = 0
-            
-            for item in raw_tasks_result:
+            for item in tasks_response:
                 task_timed = time_task(json.loads(item["result"]), item["start_time"], item["end_time"], keypress_time)
                 time_total += task_timed
-            timed_data[task["name"]] = seconds_to_time(time_total)
+            timed_data[task["name"]] = seconds_to_formatted_hours(time_total)
             time_all_tasks += time_total
         
-        timed_data["all_times"] = seconds_to_time(time_all_tasks)
-        user_record = time_tracking_users(session, user, time_tracking_url, start_time, end_time)
+        timed_data["all_times"] = seconds_to_formatted_hours(time_all_tasks)
+        user_record = time_tracking_users(session, user, time_tracking_url, start_date + 'T00:00:00', end_date + 'T23:59:59')
         merged_dir = timed_data.copy()
         merged_dir.update(user_record)
         list.append(merged_dir)
@@ -157,44 +159,39 @@ def get_final_list(users, tasks, start_time, end_time, keypress_time, task_resul
 
 def main():
     args = parse_args()
-    file = args.file
-    username = args.login
-    password = args.password
-    start_time = args.start_time
-    end_time = args.end_time
-    link = args.api_link
     keypress_time = int(args.keypress_time)
 
-    login_url = link + 'token'
-    user_url = link + 'user/'
-    task_url = link + 'task/task'
-    task_result_url = link + 'task/results'
-    time_tracking_url = link + 'time_tracking/time_tracking'
+    login_url = urljoin(args.api_url, 'token')
+    user_url = urljoin(args.api_url, 'user/')
+    task_url = urljoin(args.api_url, 'task/task')
+    task_result_url = urljoin(args.api_url, 'task/results')
+    time_tracking_url = urljoin(args.api_url, 'time_tracking/time_tracking')
 
     session = requests.Session()
 
-    session = get_session(login_url, session, username, password)
+    session = get_session(login_url, session, args.login, args.password)
     
-    if session == None:
-        print("Couldn't estabilish connection")
-        sys.exit(1) 
+    users = query_api(user_url, session, None)
 
-    users = get_result(user_url, session, None)
-
-    raw_tasks = get_result(task_url, session, None)
-    tasks = filter_tasks(raw_tasks)
+    tasks = query_api(task_url, session, None)
+    tasks = [item for item in tasks if item['active']]
     
-    final_list = get_final_list(users, tasks, start_time, end_time, keypress_time, task_result_url, time_tracking_url, session)
+    final_list = get_final_list(users, tasks, args.start_date, args.end_date, keypress_time, task_result_url, time_tracking_url, session)
 
     field_names = []
-    field_names_dict = max(final_list,key=len)
+    field_names_dict = max(final_list, key=len)
     for key in field_names_dict:
         field_names.append(key)
 
-    with open(file, 'w') as csvfile: 
-        writer = csv.DictWriter(csvfile, fieldnames = field_names) 
+    with open(args.output + '.jsonl', 'w') as csvfile:
+        for item in final_list:
+            csvfile.write(json.dumps(item) + '\n')
+
+    with open(args.output + '.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader() 
         writer.writerows(final_list) 
+
 
 if __name__ == '__main__':
     main()
