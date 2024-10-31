bc4e36b80afb528c3990683863bd534f6f0e9bf7
Michal Hradis <michal.hradis@gmail.com>
2024-10-31 10:49:51 +0100

    No training / in url.

--- backend/semant_annotation/routes/user_routes.py
+++ backend/semant_annotation/routes/user_routes.py
@@ -14,14 +14,14 @@ from semant_annotation.schemas import base_objects
 from semant_annotation.schemas.auth_objects import TokenData
 
 
-@user_route.post("/", response_model=None, tags=["User"])
+@user_route.post("", response_model=None, tags=["User"])
 async def add_user_route(user: base_objects.UserWithPasswd,
         user_token: TokenData = Depends(get_current_admin), db: AsyncSession = Depends(get_async_session)):
     user_dict = user.model_dump(exclude={'password'})
     await add_user(db, base_objects.User(**user_dict), user.password)
 
 
-@user_route.put("/", response_model=None, tags=["User"])
+@user_route.put("", response_model=None, tags=["User"])
 async def update_user(user: base_objects.User,
         user_token: TokenData = Depends(get_current_admin), db: AsyncSession = Depends(get_async_session)):
     if not user_token.trusted_user:
@@ -38,7 +38,7 @@ async def register_user(user: base_objects.UserWithPasswd,
     await add_user(db, base_objects.User(**user_dict), user.password)
 
 
-@user_route.get("/", response_model=List[base_objects.User], tags=["User"])
+@user_route.get("", response_model=List[base_objects.User], tags=["User"])
 async def get_all_users(
         user_token: TokenData = Depends(get_current_admin), db: AsyncSession = Depends(get_async_session)):
     return await crud_user.get_all_users(db)
